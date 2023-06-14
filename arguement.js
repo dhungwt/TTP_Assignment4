3fc7667cfcdcb8479e15a8c8d18a17355be0ec20
AnyiC <anyichen212@gmail.com>
2023-06-14 15:11:17 -0400

    update comments for q1 files

--- arguement.js
+++ arguement.js
@@ -1,11 +1,14 @@
+//check if first click is press
 let firstClick = false;
 
+//set up new p element to add to the click buttons
 let firstArgue = document.createElement('p');
 firstArgue.textContent = "I'm right!"
 
 let argue = document.createElement('p');
 argue.textContent = "No, I'm right!"
 
+//activate when click left button
 function buttonClick1(){
     if(!firstClick){
         document.querySelector(".b1").prepend(firstArgue);
@@ -17,6 +20,7 @@ function buttonClick1(){
     }
 }
 
+//activate when click right button
 function buttonClick2(){
     if(!firstClick){
         document.querySelector(".b2").prepend(firstArgue);
