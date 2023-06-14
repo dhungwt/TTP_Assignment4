53ac64bbca425876e4b77639c5797a1913338e13
Alina1z <136369748+Alina1z@users.noreply.github.com>
2023-06-14 22:58:52 +0400

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,11 @@
+
+
+
+let username = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+
+  console.log("Username:", username);
+  console.log("Email:", email);
+  console.log("Password:", password);
